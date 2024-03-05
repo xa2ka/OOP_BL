9dcf4d54072ff308c43de3257b9fd37aa7649ced
xa2ka <sanadovnar@gmail.com>
2024-03-06 00:43:52 +0300

    Create Rep & Serv

--- User/User.py
+++ User/User.py
@@ -1,7 +1,7 @@
 class User:
-    def __init__(self, name, surname, email, password, old_year,
+    def __init__(self, id, name, surname, email, password, old_year,
                  weight, weight_goal, cal_goal, gender, water_goal):
-        self.name, self.surname, self.email, self.password, self.old_year, \
+        self.id, self.name, self.surname, self.email, self.password, self.old_year, \
         self.weight, self.weight_goal, self.cal_goal, self.gender, self.water_goal = \
-            name, surname, email, password, old_year, weight, weight_goal, cal_goal, gender, water_goal
+            id, name, surname, email, password, old_year, weight, weight_goal, cal_goal, gender, water_goal
 
