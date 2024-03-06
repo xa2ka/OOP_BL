fc8a000d4414e4d713e3a9d4d80a8248975415bf
xa2ka <sanadovnar@gmail.com>
2024-03-06 23:42:07 +0300

    add some func

---
+++ User/UserRep.py
@@ -0,0 +1,12 @@
+class UserRep:
+
+    def __init__():
+        pass 
+    
+    def GetUserById(self, user_id):
+        pass
+    #берем юзера с бд
+
+    def WriteInDb(self,user_id):
+        pass
+    #запись в бд юзера
--- User/UserServ.py
+++ User/UserServ.py
@@ -1,25 +1,106 @@
+import UserRep
+import User
+
 class UserServ:
 
-    def change_name(self,id,new_name):       
-       pass
+    def change_name(self, user_id, new_name):       
+     
+      UserRepo = UserRep()
 
-    def change_surname():
-       pass
+      user = UserRepo.GetUserById(user_id)
+           
+      if user:
+            user.name = new_name
+      else:
+            #error   
+         
+       UserRepo.WriteInDb(user_id)
+      
 
-    def change_water_goal():
-     pass
+    def change_surname(self, user_id, new_surname):
+      
+      UserRepo = UserRep()
 
-    def change_cal_goal():
-       pass
+      user = UserRepo.GetUserById(user_id)
+           
+      if user:
+            user.surname = new_surname
+      else:
+            #error   
+     
+          
+       UserRepo.WriteInDb(user_id)
+
+    def change_water_goal(self, user_id, new_water_goal):
+      
+      UserRepo = UserRep()
+
+      user = UserRepo.GetUserById(user_id)
+      
+      if user:
+            user.water_goal = new_water_goal
+      else:
+            #error 
+       UserRepo.WriteInDb(user_id)
+
+    def change_cal_goal(self, user_id, new_cal_goal):
+      
+      UserRepo = UserRep()
+
+      user = UserRepo.GetUserById(user_id)
+      
+      if user:
+            user.cal_goal = new_cal_goal
+      else:
+            #error 
+       UserRepo.WriteInDb(user_id)
     
-    def change_gender():
-       pass
 
-    def change_old_year():
-       pass
+    def change_gender(self, user_id, new_gender):
+       
+      UserRepo = UserRep()
 
-    def change_weight_goal():
-       pass
+      user = UserRepo.GetUserById(user_id)
+      
+      if user:
+            user.gender = new_gender
+      else:
+            #error 
+       UserRepo.WriteInDb(user_id)
+
+    def change_old_year(self, user_id, new_old_year):
+       
+      UserRepo = UserRep()
+
+      user = UserRepo.GetUserById(user_id)
+      
+      if user:
+            user.old_year = new_old_year
+      else:
+            #error 
+       UserRepo.WriteInDb(user_id)
+
+    def change_weight_goal(self, user_id, new_weight_goal):
+       
+      UserRepo = UserRep()
+
+      user = UserRepo.GetUserById(user_id)
+      
+      if user:
+            user.weight_goal = new_weight_goal
+      else:
+            #error 
+       UserRepo.WriteInDb(user_id)
+
    
-    def change_weigth():
-       pass
+    def change_weigth(self, user_id, new_weight):
+       
+      UserRepo = UserRep()
+
+      user = UserRepo.GetUserById(user_id)
+      
+      if user:
+            user.weight = new_weight
+      else:
+            #error 
+       UserRepo.WriteInDb(user_id)
