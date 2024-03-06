ec41fb3a95aa294103ae478bf0fb77f9d6d7a7ed
xa2ka <sanadovnar@gmail.com>
2024-03-06 14:39:06 +0300

    direct->{entity,Rep,Serv}

---
+++ User/UserServ.py
@@ -0,0 +1,26 @@
+class UserServ:
+
+    def change_name(self,id,new_name):       
+     
+       pass
+
+    def change_surname():
+       pass
+
+    def change_water_goal():
+     pass
+
+    def change_cal_goal():
+       pass
+    
+    def change_gender():
+       pass
+
+    def change_old_year():
+       pass
+
+    def change_weight_goal():
+       pass
+   
+    def change_weigth():
+       pass
