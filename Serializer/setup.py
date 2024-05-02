4e620e74f2fdc9d36c603e92f491edcb8f9429f3
xa2ka <sanadovnar@gmail.com>
2024-05-02 17:38:09 +0300

    nothing new

--- Serializer/setup.py
+++ Serializer/setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
 
 setup(
-    name='Serializer',
+    name='serializer',
     version='2.0.0',
     description='serialization/Deserialization/Errors',
-    long_description=long_description,
     long_description_content_type='text/markdown',
     author='Xatura',
     packages=find_packages(),
